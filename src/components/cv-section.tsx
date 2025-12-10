6c362a76beb79839f2007665792bad1f2f61a6a2
valmtv <valerii.matviiv@gmail.com>
2025-12-10 21:23:07 +0000

    Files structure update for Projects

--- src/components/cv-section.tsx
+++ src/components/cv-section.tsx
@@ -8,8 +8,8 @@ import { getThemeClasses } from "lib/themes"
 import { cn } from "lib/utils"
 import { Download } from "lucide-react"
 
-import { StudentTestingProject } from "./student-testing-project"
-import { LegoAuctionProject } from "./lego-auction-project"
+import { StudentTestingProject } from "components/projects/student-testing-project"
+import { LegoAuctionProject } from "components/projects/lego-auction-project"
 
 export function CVSection() {
   const { theme } = useTheme()
