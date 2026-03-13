4bf96dc2b98a8e8c6f26ec2a4a637b0c84274119
valmtv <valerii.matviiv@gmail.com>
2026-03-13 20:53:11 +0100

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "contexts/theme-context"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,8 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SpeedInsights/>
         <ThemeProvider>{children}</ThemeProvider>
+        <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
